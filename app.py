f0ef0e434739101ab2f35d6a69a6e1fc7c0ccec8
Pham Thien Nguyen <pham.thien.nguyen@outlook.de>
2025-06-21 19:48:02 +0200

    CRUD - checked off create and read functions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
@@ -22,16 +22,38 @@ with app.app_context():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-@app.route('/about')
-def about():
-    return render_template('about.html')
-@app.route('/addpost')
-def addpost():
-    return "This is addpost"
+    """shows all blog posts ordered by date_posted"""
+    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+    return render_template('index.html', posts=posts)
 
-def post():
-    return "Here is a singular post"
+@app.route('/about/')
+def about():
+    """static HTML file with information about me"""
+    return render_template('about.html')
+
+@app.route('/post/<int:post_id>/')
+def post(post_id):
+    """shows a single blog post"""
+    post = Blogpost.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+@app.route('/add/')
+def add():
+    """opens HTML-form to create blog post"""
+    return render_template('add.html')
+
+@app.route('/addpost/', methods=['POST'])
+def addpost():
+    """for posting a blog post to the database"""
+    title = request.form['title']
+    author = request.form['author']
+    content = request.form['content']
+
+    post = Blogpost(title=title, author=author, content=content, date_posted=datetime.now())
+
+    db.session.add(post)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(port=5551, debug=True )
